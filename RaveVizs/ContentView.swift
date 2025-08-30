6c5550be734012b5f6f5f25c80fdda98efe62b00
onlyoneaman <2000.aman.sinha@gmail.com>
2025-08-31 04:00:20 +0530

    Initial Commit

---
+++ RaveVizs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RaveVizs
+//
+//  Created by Aman Kumar on 31/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RaveVizsTests/RaveVizsTests.swift
@@ -0,0 +1,17 @@
+//
+//  RaveVizsTests.swift
+//  RaveVizsTests
+//
+//  Created by Aman Kumar on 31/08/25.
+//
+
+import Testing
+@testable import RaveVizs
+
+struct RaveVizsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
